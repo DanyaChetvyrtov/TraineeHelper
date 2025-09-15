ea44a9defc8a3e67a1713e0b29837bc8424518d1
Danil <dabcom2013@gmail.com>
2025-09-15 21:29:16 +0300

    profile-ms. Add interceptor and logic with request sender data. Extract his ID, username and roles from headers.

--- services/profile-ms/build.gradle.kts
+++ services/profile-ms/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-aop")
 
 	// cloud starters
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
