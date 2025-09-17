b04676f1ad86290d5bdc6d7de31ce61045c0169e
Danil <dabcom2013@gmail.com>
2025-09-17 08:03:14 +0300

    scripts. update db-initializer. Add custom entrypoint for vault

--- scripts/db-initializer.sh
+++ scripts/db-initializer.sh
@@ -4,4 +4,6 @@ set -e
 
 echo "PostgreSQL is ready. Creating databases..."
 
-psql -U "$POSTGRES_USER" -c "CREATE DATABASE auth_user_db;" || echo "Database 'auth_user_db' already exists"
+for cur_db in "auth_user_db" "profile_db"; do
+  psql -U "$POSTGRES_USER" -c "CREATE DATABASE ${cur_db};" || echo "Database '${cur_db}' already exists"
+done
